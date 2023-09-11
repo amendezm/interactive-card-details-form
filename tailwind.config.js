9029c11c49046dd08faf94ba00a354b09bd23457
amendezm <alexis.david.mendez@gmail.com>
2023-09-11 17:52:48 -0400

    chore: configure custom colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,14 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "light-grayish-violet": "hsl(270, 3%, 87%)",
+        "dark-grayish-violet": "hsl(279, 6%, 55%)",
+        "very-dark-violet": "hsl(278, 68%, 11%)",
+        red: "hsl(0, 100%, 66%)"
+      }
+    }
   },
-  plugins: [],
-};
+  plugins: []
+}
